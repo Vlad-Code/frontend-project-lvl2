568174cc979fd55d87d341296a32308b22aba2a9
Vladislav Kungurov <vladislavkun27@gmail.com>
2022-07-30 11:06:01 +0300

    add a feature for the comparison of flat json files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,13 +2,17 @@
 
 import { Command } from 'commander';
 
+import getDiff from '../flatGendiff.js';
+
 const program = new Command();
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
-  .arguments('<filepath1> <filepath2>'); 
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => getDiff(filepath1, filepath2));
+  
 
 program
   .option('-f, --format <type>', 'output format');
---
+++ flatGendiff.js
@@ -0,0 +1,33 @@
+import process from 'process';
+import path from 'node:path';
+import { readFileSync } from 'node:fs';
+import _ from 'lodash';
+
+const getDiff = (filePath1, filePath2) => {
+  const cwd = process.cwd();
+  const absoluteFilePath1 = path.resolve(cwd, filePath1);
+  const absoluteFilePath2 = path.resolve(cwd, filePath2);
+  const file1 = readFileSync(absoluteFilePath1);
+  const file2 = readFileSync(absoluteFilePath2);
+  const fileData1 = JSON.parse(file1);
+  const fileData2 = JSON.parse(file2);
+  const keys1 = Object.keys(fileData1);
+  const keys2 = Object.keys(fileData2);
+  const unionKeys = _.union(keys1, keys2).sort();
+  const result = unionKeys.reduce((acc, key) => {
+    if (!Object.hasOwn(fileData1, key)) {
+      acc.push(`+ ${key}: ${fileData2[key]}`);
+    } else if (!Object.hasOwn(fileData2, key)) {
+      acc.push(`- ${key}: ${fileData1[key]}`);
+    } else if (fileData1[key] !== fileData2[key]) {
+      acc.push(`- ${key}: ${fileData1[key]}`);
+      acc.push(`+ ${key}: ${fileData2[key]}`);
+    } else if (fileData1[key] === fileData2[key]) {
+      acc.push(`  ${key}: ${fileData1[key]}`);
+    }
+    return acc;
+  }, []);
+  console.log(`{\n  ${result.join('\n  ')}\n}`);
+};
+
+export default getDiff;
