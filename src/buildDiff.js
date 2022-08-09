1d6006353ab9962cb30ff6316fec0f96aca5cc64
Vladislav Kungurov <vladislavkun27@gmail.com>
2022-08-09 13:11:08 +0300

    continue refactoring

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -7,15 +7,24 @@ const buildDiff = (data1, data2) => {
   const sortedUnionKeys = _.sortBy(unionKeys, ((key) => key));
   const differense = sortedUnionKeys.reduce((acc, key) => {
     if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      acc[key] = buildDiff(data1[key], data2[key]);
-    } else if (!Object.hasOwn(data1, key)) {
-      acc[key] = ['added', "doesn't exist", data2[key]];
-    } else if (!Object.hasOwn(data2, key)) {
-      acc[key] = ['deleted', data1[key], "doesn't exist"];
-    } else if (data1[key] === data2[key]) {
-      acc[key] = ['unchanged', data1[key], data2[key]];
-    } else if (data1[key] !== data2[key]) {
-      acc[key] = ['changed', data1[key], data2[key]];
+      // acc[key] = buildDiff(data1[key], data2[key]);
+      return { ...acc, [key]: buildDiff(data1[key], data2[key]) };
+    }
+    if (!Object.hasOwn(data1, key)) {
+      // acc[key] = ['added', "doesn't exist", data2[key]];
+      return { ...acc, [key]: ['added', "doesn't exist", data2[key]] };
+    }
+    if (!Object.hasOwn(data2, key)) {
+      // acc[key] = ['deleted', data1[key], "doesn't exist"];
+      return { ...acc, [key]: ['deleted', data1[key], "doesn't exist"] };
+    }
+    if (data1[key] === data2[key]) {
+      // acc[key] = ['unchanged', data1[key], data2[key]];
+      return { ...acc, [key]: ['unchanged', data1[key], data2[key]] };
+    }
+    if (data1[key] !== data2[key]) {
+      // acc[key] = ['changed', data1[key], data2[key]];
+      return { ...acc, [key]: ['changed', data1[key], data2[key]] };
     }
     return acc;
   }, {});
