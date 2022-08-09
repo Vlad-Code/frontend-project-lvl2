2b67a7043156d3c82aaba1e0dc880f54f5eddf07
Vladislav Kungurov <vladislavkun27@gmail.com>
2022-08-09 12:21:45 +0300

    replace the metjod sort on sortby in the buildDiff.js

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -3,8 +3,9 @@ import _ from 'lodash';
 const buildDiff = (data1, data2) => {
   const keys1 = Object.keys(data1);
   const keys2 = Object.keys(data2);
-  const unionKeys = _.union(keys1, keys2).sort();
-  const differense = unionKeys.reduce((acc, key) => {
+  const unionKeys = _.union(keys1, keys2);
+  const sortedUnionKeys = _.sortBy(unionKeys, ((key) => key));
+  const differense = sortedUnionKeys.reduce((acc, key) => {
     if (_.isObject(data1[key]) && _.isObject(data2[key])) {
       acc[key] = buildDiff(data1[key], data2[key]);
     } else if (!Object.hasOwn(data1, key)) {
